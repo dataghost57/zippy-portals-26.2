ce5854f6661be8500df2007372d64ea4719c8386
DataGhost57 <jrayplays333444@gmail.com>
2026-09-07 07:18:06 -0700

    update

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # Zippy Portals
 
-## Setup
-
-For setup instructions, please see the [Fabric Documentation page](https://docs.fabricmc.net/develop/getting-started/creating-a-project#setting-up) related to the IDE that you are using.
-
-## License
-
-This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
+A simple mod that makes traveling through nether portals instant in survival mode.
+## Credits
+Developer: DataGhost57
--- src/main/java/com/jacobray/zippyportals/mixin/ExampleMixin.java
+++ src/main/java/com/jacobray/zippyportals/mixin/ExampleMixin.java
@@ -1,15 +1,27 @@
 package com.jacobray.zippyportals.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.block.Portal;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(MinecraftServer.class)
+@Mixin(targets = "net.minecraft.world.entity.PortalProcessor")
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
+
+	@Redirect(
+			method = "processPortalTeleportation",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/world/level/block/Portal;getPortalTransitionTime(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/Entity;)I"
+			)
+	)
+	private int zippyPortals$instantPortal(
+			Portal portal,
+			ServerLevel level,
+			Entity entity
+	) {
+		return 0;
 	}
 }
